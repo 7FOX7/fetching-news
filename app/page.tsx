92ad1fa0d9e2c97fc91afff389270157009ea191
7FOX7 <kheeugene@gmail.com>
2024-10-22 18:12:40 -0400

    Implemented pagination logic

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import { getSelection } from "./lib/actions"
 import { getData } from "./lib/actions"
 import { redirect } from "next/navigation"
 import { AnimalData } from "./lib/definitions"
+import Pagination from "./pagination"
+import Images from "./ui/images"
+import LatestNews from "./ui/latest-news"
 
 export default async function Home() {
    const selection = await getSelection()
@@ -12,8 +15,23 @@ export default async function Home() {
 
    const data = await getData();
    const animalData: AnimalData = data[selection.value][0]
-   
+
    return (
-      <p>{animalData.description}</p>
+      <section className="w-full bg-red-500 grid grid-cols-1">
+         <div className="bg-blue-500 grid grid-cols-1 md:grid-cols-2">
+            <Images
+               image1={animalData.image1}
+               image2={animalData.image2}
+            />
+            <LatestNews news={animalData.news} />
+         </div>
+         <div>
+            <h2>Did you know...</h2>
+            <div>
+               <p>{animalData.description}</p>
+            </div>
+         </div>
+         <Pagination />
+      </section>
    )
 }
---
+++ app/pagination.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import { useSearchParams } from "next/navigation"
+import { usePathname } from "next/navigation"
+import PageArrows from "./ui/PageArrows";
+
+export default function Pagination() {
+   const searchParams = useSearchParams();
+   console.log('the original searchParams: ' + searchParams) 
+   const pathname = usePathname(); 
+   const currentPage = Number(searchParams.get('page')) || 1 
+
+   function createPage(page: number) {
+      const params = new URLSearchParams(searchParams)
+      params.set('page', page.toString())
+
+      console.log('updated search params: ' + `${pathname}?${params}`)
+      return `${pathname}?${params}`
+   }
+   return (
+      <PageArrows 
+         prevPage={createPage(currentPage - 1)}
+         nextPage={createPage(currentPage + 1)}
+         leftArrowIsDisabled={currentPage <= 1}
+         rightArrowIsDisabled={currentPage >= 2}
+      />
+   )
+}
